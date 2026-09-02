8657505ec31c0307b06a84f5147d2cf507028fcb
nemnis <nemnis@protonmail.com>
2026-09-02 13:30:33 +0100

    jekyll install and test on index

---
+++ _site/web/views/common/head.php
@@ -0,0 +1,19 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Luis Lugo - Web Designer & Developer</title>
+
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TSKT6SZM');</script>
+<!-- End Google Tag Manager -->
+
+<link rel="icon" type="image/png" href="./web/images/favicon.png">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="./web/styles/style.css">
+<link rel="stylesheet" type="text/css" href="./web/styles/nav.css">
+<?php include './web/views/common/og.php' ?>
